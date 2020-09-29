d7f341eeb5328e47059f294b24f6b7e050ebd6b8
kingwederson <56653848+kingwederson@users.noreply.github.com>
2020-09-29 18:44:35 -0300

    Add files via upload

---
+++ cabecalho2.js
@@ -0,0 +1,28 @@
+function cabecalho2(){
+    var cabecalho = window.document.getElementById("espacocabecalho")
+    cabecalho.innerHTML = `
+    <div class="cabe">
+        <table>
+            <tr>
+                <td>
+                    <img src="../santinho.png" class="capitular">
+                </td>
+                <td>
+                    <header>
+                        <img src="../letreiro.svg" id="wordmark">
+                    </header>
+                </td>
+            </tr>
+        </table>
+        <nav class="menu">
+            <ul>
+                <a href="../index.html"><li>Principal</li></a>
+                <a href="../perguntas/"><li>Perguntas</li></a>
+                <a href="../apoio/"><li>Apoio</li></a>
+                <a href="../depoimentos/"><li>Depoimentos</li></a>
+            </ul>
+        </nav>
+        <p class="contato">Mande um email para <b>contato@wederson.net.br</b> ou uma mensagem via <a href="https://api.whatsapp.com/send?phone=5532999185541"><b>Whatsapp</b></a> ou Telegram para <b>+55 32 999 185 541</b>.</p>
+    </div>
+    `
+}
